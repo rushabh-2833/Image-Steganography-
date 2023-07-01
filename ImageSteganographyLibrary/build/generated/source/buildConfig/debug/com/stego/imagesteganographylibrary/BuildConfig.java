8480d88ce59a95a7436271049c5da0fde2250d10
Rushabh <125784665+rushabh-2833@users.noreply.github.com>
2023-07-01 19:17:00 +0530

    Add files via upload

---
+++ ImageSteganographyLibrary/build/generated/source/buildConfig/debug/com/stego/imagesteganographylibrary/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.stego.imagesteganographylibrary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.stego.imagesteganographylibrary";
+  public static final String BUILD_TYPE = "debug";
+}
